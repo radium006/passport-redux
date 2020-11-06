9673c00874edbb8687c69d2367d207667506b246
Kevin Parmer <jkparmer89@gmail.com>
2020-11-05 20:53:00 -0600

    initialized frontend

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,26 @@
+
 import express from 'express'
+import User from '../models/user'
+import { signUp } from '../validations/user'
 
-const userRoutes = express.Router()
+const userRoutes = express.Router();
 
-userRoutes.post("", (req, res) => {
-    res.send(req.body)
+userRoutes.post("", async (req, res) => {
+    console.log(req.body)
+    try {
+        
+        const { username, email, password } = req.body
+        
+        await signUp.validate({username, email, password})
+        console.log(username)
+        const newUser = new User({username, email, password});
+        
+        await newUser.save();
+        res.send({userId: newUser.id, username})
+    } catch(e) {
+        console.log(e)
+        res.send(e)
+    }
 })
 
 export default userRoutes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,44 @@
-import express from 'express'
-import {PORT, NODE_ENV} from './config'
+import express from "express";
+import mongoose from "mongoose";
+import session from 'express-session'
+import connectStore from 'connect-mongo'
+import { userRoutes } from "./routes/index";
+import { PORT, NODE_ENV, MONGO_URI, SESS_NAME, SESS_SECRET, SESS_LIFETIME } from "./config";
 
-const app = express()
-//Line below obscures public users that express is being used in the backend
-app.disable('x-powered-by')
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+    console.log("MongoDB Connected");
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json)
+    const app = express();
+    //Line below obscures public users that express is being used in the backend
+    app.disable("x-powered-by");
 
-const apiRouter = express.Router
-app.use('/api', apiRouter)
-apiRouter.use('/users', userRoutes)
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(session({
+        name: SESS_NAME,
+        secret: SESS_SECRET,
+        saveUninitialized: false,
+        resave: false,
+        store: new MongoStore({
+          mongooseConnection: mongoose.connection,
+          collection: 'session',
+          ttl: parseInt(SESS_LIFETIME) / 1000
+        }),
+        cookie: {
+          sameSite: true,
+          secure: NODE_ENV === 'production',
+          maxAge: parseInt(SESS_LIFETIME)
+        }
+      }));
 
-app.listen(PORT, () => console.log(`Listing on port ${PORT}`))
+    const apiRouter = express.Router();
+    app.use("/api", apiRouter);
+    apiRouter.use("/users", userRoutes);
+
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+})();
